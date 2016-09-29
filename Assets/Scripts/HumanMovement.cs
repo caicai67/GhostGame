c784149e79e3e9a786431dca73266cb821e80e01
caicai67 <caitlinmorris@Caitlins-MacBook-Pro.local>
2016-09-29 19:49:29 -0400

    added movement for human

---
+++ Assets/Scripts/HumanMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HumanMovement : MonoBehaviour {
+
+	public float speed = 10f;
+
+	private Rigidbody2D body;
+	private float horz;
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		horz = Input.GetAxis ("Horizontal");
+		body.AddForce (Vector2.right * horz * speed);
+		Debug.Log (horz);
+	}
+}
