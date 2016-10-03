6ed1d17a3690cb9ccb8b4ba7c853775f467f4595
Mariam Naziripour <MariamJ@Sugar-Pie.local>
2016-10-02 20:40:43 -0400

    Added collectable items

---
+++ Assets/Collectable.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collectable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+		
+	void OnCollisionStay2D (Collision2D collision)
+	{
+		Destroy (this.gameObject);
+		print ("WOW");
+
+	}
+}
