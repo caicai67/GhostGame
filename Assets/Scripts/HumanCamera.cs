723be3f0d69f46c8260cda1c62e851360d8e06cb
caicai67 <caitlinmorris@Caitlins-MacBook-Pro.local>
2016-10-02 23:00:50 -0400

    changed human camera

--- Assets/Scripts/HumanCamera.cs
+++ Assets/Scripts/HumanCamera.cs
@@ -1,14 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerScript : MonoBehaviour {
+public class HumanCamera : MonoBehaviour {
+
+	private SpriteRenderer roomR;
 
 	// Use this for initialization
 	void Start () {
+		roomR = GameObject.Find("House").transform.Find("Bedroom 2").GetComponent<SpriteRenderer>();
 	}
-	
 	// Update is called once per frame
 	void Update () {
-	
+		Vector3 roomMin = roomR.bounds.min;
+		Vector3 roomMax = roomR.bounds.max;
+		Debug.Log ("room min " + roomMin);
+		Debug.Log ("room max " + roomMax);
 	}
 }
