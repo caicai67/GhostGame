b3b55304cc7d5eab4546d7f17fbbed74e8ab71cd
caicai67 <caitlinmorris@lawn-128-61-126-42.lawn.gatech.edu>
2016-10-04 12:16:05 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Assets/Scenes/MainScene.unity

--- Assets/Scripts/HumanCamera.cs
+++ Assets/Scripts/HumanCamera.cs
@@ -3,17 +3,11 @@ using System.Collections;
 
 public class HumanCamera : MonoBehaviour {
 
-	private SpriteRenderer roomR;
-
 	// Use this for initialization
 	void Start () {
-		roomR = GameObject.Find("House").transform.Find("Bedroom 2").GetComponent<SpriteRenderer>();
 	}
 	// Update is called once per frame
 	void Update () {
-		Vector3 roomMin = roomR.bounds.min;
-		Vector3 roomMax = roomR.bounds.max;
-		Debug.Log ("room min " + roomMin);
-		Debug.Log ("room max " + roomMax);
+		
 	}
 }
