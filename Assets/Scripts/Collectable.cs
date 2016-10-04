ea1bf8de29f90ce8ad6b1343e8c6b34468f8ffb5
Mariam Naziripour <MariamJ@lawn-128-61-71-7.lawn.gatech.edu>
2016-10-04 12:18:04 -0400

    Walls and collectable item scripts

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Collectable : MonoBehaviour {
+
+	public string collectedItem;
+	public bool collected;
+	public Image uiSpriteReg;
+	public Sprite uiSpriteCollected;
+
+	// Use this for initialization
+	void Start () {
+		collected = false;
+		//collectedItem = this.gameObject.name;
+		//uiSpriteReg = GameObject.Find ("RoseUI");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+	void OnCollisionStay2D (Collision2D collision)
+	{
+		//collectedItem = ;
+		collected = true;
+		if (collected == true) {
+			uiSpriteReg.sprite = uiSpriteCollected;
+		}
+		Destroy (this.gameObject);
+
+	}
+}
--- Assets/Scripts/Collected.cs
+++ Assets/Scripts/Collected.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Collectable : MonoBehaviour {
+public class Collected : MonoBehaviour {
+
+
 
 	// Use this for initialization
 	void Start () {
@@ -10,13 +12,9 @@ public class Collectable : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
-		
-	void OnCollisionStay2D (Collision2D collision)
-	{
-		Destroy (this.gameObject);
-		print ("WOW");
 
+
+			
+	
 	}
 }
